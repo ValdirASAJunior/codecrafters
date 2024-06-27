297150f3213522224704021a64af275717a0e542
thal-ia <thalia.nunes12@hotmail.com>
2024-06-27 10:54:07 -0300

    Projetos Ícones Estruturados

--- src/componentes/Projetos.js
+++ src/componentes/Projetos.js
@@ -5,6 +5,9 @@ function Projetos() {
       <div className="Projetos">
         <div class="caixaOpacidade">
           <div class="caixaTexto2"><p>Projetos</p></div>
+          <div class="jogo1"></div>
+          <div class="jogo2"></div>
+          <div class="jogo3"></div>
         </div>
       </div>
     );
