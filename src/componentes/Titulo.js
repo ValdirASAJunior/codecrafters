52c832a83028417217d0c4a63f47d8de1f7cfb79
ValdirASAJunior <valdirjuniorprincipal@gmail.com>
2024-06-27 09:31:43 -0300

    Botões Devs Funcionando

--- src/componentes/Titulo.js
+++ src/componentes/Titulo.js
@@ -1,16 +1,47 @@
 import React from "react";
+import goodra from "../imagens/Goodra.webp";
+import ditto from "../imagens/ditto.jpg";
 
 function Titulo() {
+
+  function TrocarE(){
+    const dev = document.getElementById("caixaDevs");
+    const btnEsq = document.getElementById("trocarFundo");
+    const btnDir = document.getElementById("trocarFundo2");
+
+      // Verifica qual é o plano de fundo atual e alterna para o próximo
+      if (dev.className = "dev2") 
+      {
+        dev.className = "devs";
+        btnEsq.classList.toggle("show2");
+        btnDir.classList.toggle("hide");
+      }
+    };
+
+    function TrocarD(){
+      const dev = document.getElementById("caixaDevs");
+      const btnEsq = document.getElementById("trocarFundo");
+      const btnDir = document.getElementById("trocarFundo2");
+  
+        // Verifica qual é o plano de fundo atual e alterna para o próximo
+        if (dev.className = "devs") 
+          {
+            dev.className = "dev2";
+            btnEsq.classList.toggle("show2");
+            btnDir.classList.toggle("hide");
+          } 
+        };
+
     return (
       <div className="Titulo">
         <div class="logo"></div>
         <div class="title"><h1>Reinventando a<br/>Inovação</h1></div>
 
-        <div class="devs">
+        <div class="devs" id="caixaDevs">
           <p class="txtEquipe">Equipe</p>
           <div class="caixaEquipe"></div>
-          <button class="setaECaixa"></button>
-          <button class="setaDCaixa"></button>
+          <button class="setaECaixa" id="trocarFundo" onClick={TrocarE}></button>
+          <button class="setaDCaixa" id="trocarFundo2" onClick={TrocarD}></button>
           <div class="nomeCaixa"><p>Nome</p></div>
         </div>
       </div>
