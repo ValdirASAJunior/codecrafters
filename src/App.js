cb56d9a2241a157840f16e4a024893493cda1e8d
ValdirASAJunior <valdirjuniorprincipal@gmail.com>
2024-07-03 15:39:09 -0300

    Animação no Fundo 100% Funcional

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Projetos from './componentes/Projetos';
 function App() {
   return (
     <div className="App">
+      <div className="estrelas1"></div>
+      <div className="estrelas2"></div>
       <Navbar/>
       <Titulo/>
       <Projetos/>
